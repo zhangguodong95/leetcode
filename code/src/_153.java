27f0a817d729c72b838b35146aa1a86159f43e81
zhangguodong <2226007790@qq.com>
2020-05-28 10:31:25 +0800

    添加153精简版

--- code/src/_153.java
+++ code/src/_153.java
@@ -4,53 +4,19 @@
  * @date 2020/5/25 15:26
  */
 public class _153 {
-    //方法一
-//    public int findMin(int[] nums) {
-//        //特殊情况
-//        if (nums.length == 1) {
-//            return nums[0];
-//        }
-//        int start = 0;
-//        int end = nums.length - 1;
-//        //没有旋转的情况
-//        if (nums[start] < nums[end]) {
-//            return nums[start];
-//        }
-//        //有旋转
-//        while (start <= end) {
-//            int mid = (start + end) >> 1;
-//            //判断mid是否是旋转点
-//            if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
-//            if (nums[mid - 1] > nums[mid]) return nums[mid];
-//            //寻找旋转点所在的区域
-//            if (nums[0] < nums[mid]) {
-//                start = mid ;
-//            } else {
-//                end = mid;
-//            }
-//        }
-//        return -1;
-//    }
 
-    /**
-     * 方法二
-     * @param nums
-     * @return
-     */
     public int findMin(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
+        //寻找 left == mid == right的点 就是要找的最小点
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            }
-            if (nums[mid] < nums[right]) {
-                right = mid;
+                left = mid + 1;//无序在右边 且一定不是mid点
+            } else {
+                right = mid;//无序在左边 有可能是mid点
             }
         }
-
-        if (left == right) return left;
-        return -1;
+        return left;
     }
 }
