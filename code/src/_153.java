2c49bdafe3063b07c4865962e39f87a6a5513004
zhangguodong <2226007790@qq.com>
2020-06-27 22:59:43 +0800

    6.27

--- code/src/_153.java
+++ code/src/_153.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 /**
  * @author zgd
  * @version 1.0
@@ -5,18 +7,26 @@
  */
 public class _153 {
 
+    @Test
+    public void test() {
+        int[] nums = {3, 4, 5, 1, 2};
+        System.out.println(findMin(nums));
+    }
+
     public int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        //寻找 left == mid == right的点 就是要找的最小点
-        while (left < right) {
+        int left = 0, right = nums.length - 1;
+        //当left == right的时候就是最小值的时候
+        while (left <right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;//无序在右边 且一定不是mid点
-            } else {
-                right = mid;//无序在左边 有可能是mid点
+            if (nums[mid] > nums[right]) {//表示右边是无序的 转折点一定在右边 同时一定不是Mid
+                left = mid + 1;
+            } else {//表示左边是无序的 在左边 同时mid有可能就是最小点
+                right = mid;
             }
         }
-        return left;
+        return nums[right];
     }
+
+
+
 }
--- code/src/_33.java
+++ code/src/_33.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 /**
  * @author zgd
  * @version 1.0
@@ -11,35 +13,36 @@ public class _33 {
         3.在有序数组中使用二分查找 查找固定值
      */
 
-    public static void main(String[] args) {
+    //直接使用二分查找
+    @Test
+    public void test() {
         int[] nums = {1, 3};
-        int search = search(nums, 3);
-        System.out.println(search);
+        int target = 3;
+        System.out.println(search(nums,3));
     }
-    public static int search(int[] nums, int target) {
-        int min_index = findMin(nums);
-        if (nums[nums.length - 1] == target) return nums.length - 1;
-        int left = nums[nums.length - 1] < target ? 0 : min_index;
-        int right = nums[nums.length -1] >target ? (nums.length - 1) : min_index;
-
+    public  int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if (target == nums[mid]) return mid;
+            //首先判断左边有序还是右边有序
+            if (nums[mid] >= nums[left]) {//左边有序
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
-
         return -1;
     }
 
-    public static int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) left = mid + 1;
-            else right = mid;
-        }
-        return left;
-    }
+
+
 }
